f3413931bdcb8c5927d59d9e7afcb654389b65f6
adam-arold <arold.adam@gmail.com>
2021-10-27 00:01:32 +0200

    Fix prisma-generate 😒

--- script/prisma-generate.sh
+++ script/prisma-generate.sh
@@ -2,14 +2,12 @@
 
 cd "$(dirname "$0")"
 
-source projects.sh
-
 if [ -z "$1" ]; then
     print_usage "Project name is missing!"
 fi
 
 cd ..
 
-pushd ${projects[$1]}
+pushd apps/$1
 npx prisma generate
 popd
