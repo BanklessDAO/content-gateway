c128bd5228e4286f138176bc0e187f28267254da
adam-arold <arold.adam@gmail.com>
2021-10-27 10:19:42 +0200

    Add custom Prisma deploy script because Heroku is stupid and moves node_modules around 😫

---
+++ script/prisma-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+source projects.sh
+
+if [ -z "$1" ]
+then
+    print_usage "Project name is missing!"
+fi
+
+cd ..
+
+pushd ${projects[$1]}
+
+workdir=$(pwd)
+echo "Workdir is: $workdir"
+
+npx prisma generate
+npx prisma migrate deploy
+
